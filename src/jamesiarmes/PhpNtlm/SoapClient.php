05cc5850a6c844e00abfbe777d50a6f4bb1030e9
James Armes <james.armes@acquia.com>
2016-11-28 23:24:34 -0500

    Initial commit of the SoapClient class.

---
+++ src/jamesiarmes/PhpNtlm/SoapClient.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Contains \JamesIArmes\PhpNtlm\NTLMSoapClient.
+ */
+
+namespace jamesiarmes\PhpNtlm;
+
+/**
+ * Soap Client using Microsoft's NTLM Authentication.
+ *
+ * @package php-ntlm\Soap
+ */
+class SoapClient extends \SoapClient
+{
+    /**
+     * cURL resource used to make the SOAP request
+     *
+     * @var resource
+     */
+    protected $ch;
+
+    /**
+     * Options passed to the client constructor.
+     *
+     * @var array
+     */
+    protected $options;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct($wsdl, array $options = null)
+    {
+        // Set missing indexes to their default value.
+        $options += array(
+            'user' => null,
+            'password' => null,
+            'curlopts' => array(),
+            'validate_cert' => true,
+        );
+        $this->options = $options;
+
+        // Verify that a user name and password were entered.
+        if (empty($options['user']) || empty($options['password'])) {
+            throw new \BadMethodCallException(
+                'A username and password is required.'
+            );
+        }
+
+        parent::__construct($wsdl, $options);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __doRequest($request, $location, $action, $version, $one_way = 0)
+    {
+        $headers = $this->buildHeaders($action);
+        $this->__last_request_headers = $headers;
+
+        // Only reinitialize curl handle if the location is different.
+        if (!$this->ch
+            || curl_getinfo($this->ch, CURLINFO_EFFECTIVE_URL) != $location) {
+            $this->ch = curl_init($location);
+        }
+
+        curl_setopt_array($this->ch, $this->curlOptions($action, $request));
+        $response = curl_exec($this->ch);
+
+        // TODO: Add some real error handling.
+        // If the response if false than there was an error and we should throw
+        // an exception.
+        if ($response === false) {
+            throw new EWS_Exception(
+                'Curl error: ' . curl_error($this->ch),
+                curl_errno($this->ch)
+            );
+        }
+
+        return $response;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __getLastRequestHeaders()
+    {
+        return implode('n', $this->__last_request_headers) . "\n";
+    }
+
+    /**
+     * Builds the headers for the request.
+     *
+     * @param string $action
+     *   The SOAP action to be performed.
+     */
+    protected function buildHeaders($action)
+    {
+        return array(
+            'Method: POST',
+            'Connection: Keep-Alive',
+            'User-Agent: PHP-SOAP-CURL',
+            'Content-Type: text/xml; charset=utf-8',
+            "SOAPAction: \"$action\"",
+        );
+    }
+
+    /**
+     * Builds an array of curl options for the request
+     *
+     * @param string $action
+     *   The SOAP action to be performed.
+     * @param string $request
+     *   The XML SOAP request.
+     * @return array
+     *   Array of curl options.
+     */
+    protected function curlOptions($action, $request)
+    {
+        $options = $this->options['curlopts'] + array(
+            CURLOPT_SSL_VERIFYPEER => $this->options['validate_cert'],
+            CURLOPT_SSL_VERIFYHOST => $this->options['validate_cert'],
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HTTPHEADER => $this->buildHeaders($action),
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_HTTPAUTH => CURLAUTH_BASIC | CURLAUTH_NTLM,
+            CURLOPT_USERPWD => $this->user . ':' . $this->password
+        );
+
+        // We shouldn't allow these options to be overridden.
+        $options[CURLOPT_POST] = true;
+        $options[CURLOPT_POSTFIELDS] = $request;
+
+        return $options;
+    }
+}
